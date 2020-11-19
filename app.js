07ebcec1a4ac9d9f8f174af4965398a621021201
Kama580 <kleetal@gmail.com>
2020-11-19 13:04:17 -0500

    first part

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-//updating something
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
